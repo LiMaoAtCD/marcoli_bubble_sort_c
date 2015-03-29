a6010ed2a63b73e99413545b531d5a71bfd92870
liamscofield <liamscofield@gmail.com>
2015-03-29 18:56:20 +0800

    Initial Commit

---
+++ C_/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C_
+//
+//  Created by AlienLi on 15/3/29.
+//  Copyright (c) 2015年 MCL. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
