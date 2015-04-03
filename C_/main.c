7c362f1c7a91a054205937588beaa9178fe5a93d
liamscofield <liamscofield@gmail.com>
2015-04-03 10:01:46 +0800

    bubble_sort
    a instance simply implement bubble sort

--- C_/main.c
+++ C_/main.c
@@ -8,8 +8,43 @@
 
 #include <stdio.h>
 
+void bubble_sort(int *arr, int len);
+void swap(int *, int *);
+
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+
+    int a[] = {9,4,4,3,1};
+    
+    bubble_sort(a,5);
+    for (int i =0; i< 5; i++) {
+        printf("%d",a[i]);
+
+    }
+    
     return 0;
+}
+
+
+void bubble_sort(int *arr, int len) {
+    
+    for (int i = 0 ; i < len - 1; i++) {
+        
+        for (int j = 0 ; j < len - 1 - i; j++) {
+            
+            if (*(arr+j) > *(arr+j+1)) {
+                swap((arr+j), ((arr+1+j)));
+            }
+            
+        }
+        
+    }
+}
+
+void swap(int *a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+    
+    
 }
